625e114e54e2c7c99095219dea9eb6bca8168ce8
Andre Maia Chagas <andremaia.chagas@gmail.com>
2023-04-26 10:04:08 +0100

    Update PC_script.py

--- code/PC_script.py
+++ code/PC_script.py
@@ -5,11 +5,7 @@ import class_li7000
 
 
 """Licor serial comm is done with 9600 baud, 8 data bits, no parity, 1 stop bit (9600 N 8 1) configured as a DTE device
-- XOFF (0x13) indicates full input buffer
-- XON (0x11) when input buffer is free again
-commands ARE case sensitive
-commands start with "(" and must end with ")"
-line feed character (decimal 10 <lf>) must follow commmands send to the analyzer
+
 """
 
 """
@@ -32,27 +28,5 @@ Licor temperature, co2, water for both chambers (check getting the difference fr
 
 """
 
-
-
-li7=class_li7000.li7000(port = "COM1", baudrate=115200,time=1)
-print(li7.li7000_header())
-
-
-
-### calibration of H2O and CO2
-
-#zero water cell A
-li7.li7000_zeroh2o(span_interval=0.1)#span interval in minutes
-#match  cell b equal to cell A
-li7.li7000_matchH2O()
-#get result of calibration
-h20Cal=li7.li7000_h20calresult()
-
-
-#zero c02 water cell A
-li7.li7000_zeroco2(span_interval = 0.1)
-#match cell B to cell A
-li7.li7000_matchCO2()
-#get result of calibration
-CO2Cal=li7.li7000_co2calresult()
-
+class li850:
+    pass
